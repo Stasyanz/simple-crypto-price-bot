0d7591d3c540cd1afe0069795fb56c854796b861
dev <stasyanzayac@gmail.com>
2022-07-07 13:15:20 +0300

    move currency to outer scope
    add comments

--- src/main.py
+++ src/main.py
@@ -11,14 +11,16 @@ env.read_env()
 bot = TeleBot(os.getenv("BOT_TOKEN"))
 api = CoinGeckoAPI()
 
+currency = "usd"
+
 
 @bot.message_handler(content_types=["text"])
 def crypto_price(message) -> None:
     """
     This handler will be called when user sends any text
     """
-    crypto = message.text
-    currency = "usd"
+    crypto = message.text  # currency full name
+
     response = api.get_price(ids=crypto, vs_currencies=currency)
     if not response:
         bot.send_message(message.chat.id, "Crypto not found, try another name")
